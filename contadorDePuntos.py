10400e1f9ede469a22c78d9a51caac4ca63507ad
David Adrian Martinez Saldaña <davidadrian272019@hotmail.com>
2020-10-06 15:28:49 -0500

    archivos txt y menu

---
+++ contadorDePuntos.py
@@ -0,0 +1,4 @@
+from radio import radio
+from sales import sales
+from streaming import streaming 
+
---
+++ menu.py
@@ -0,0 +1,14 @@
+from radio import radio
+from sales import sales
+from streaming import streaming 
+
+while True:
+
+    print("BIENVENIDOS AL BILLBOARD HOT 100")
+    print("1.-'Contar los puntos de una canción' 2.-'Ver el top de canciones' 3.-'Salir del programa'")
+
+    opcionInicial = int(input("Elija una opción: "))
+
+    if opcionInicial == 1
+
+
